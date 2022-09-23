e343505580aaca71d5c06cd64b1feb5396b9ea57
dewinor <kanopus05@mail.ru>
2022-09-23 23:25:42 +0700

    Задание №1 - Забыл добавить проверку, содержится ли 4 во входном массиве. Если 4 нет, то выбрасывается RuntimeException. Добавлен отдельный тест, для проверки получения исключения. Переименовал методы в более понятные.

--- src/homework6/MainTest.java
+++ src/homework6/MainTest.java
@@ -8,15 +8,24 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class MainTest {
 
     @CsvSource(value = {
+            "1,2,0,0,2,3,0,1,7",
+            "1,2,3,5,6,1"
+    })
+    @ParameterizedTest
+    public void getNumbersFromArrayAfterLastFourTestException(@ConvertWith(IntegerArrayConverter.class) Integer[] oldArray) {
+        Assertions.assertThrows(RuntimeException.class, () -> Main.getNumbersFromArrayAfterLastFour(oldArray));
+    }
+
+    @CsvSource(value = {
             "1,2,4,4,2,3,4,1,7;1,7",
             "20,1,4,51,5,25,4,12,15,2,1,56;12,15,2,1,56",
             "4,1,2,3;1,2,3",
-            "1,2,3,5,3,4,1; 1"
+            "1,2,3,5,3,4,1;1",
     }, delimiterString = ";")
     @ParameterizedTest
-    public void createNewArrayBasedOldTest(@ConvertWith(IntegerArrayConverter.class) Integer[] oldArray,
-                                           @ConvertWith(IntegerArrayConverter.class) Integer[] newArray) {
-        Assertions.assertArrayEquals(newArray, Main.createNewArrayBasedOld(oldArray));
+    public void getNumbersFromArrayAfterLastFourTest(@ConvertWith(IntegerArrayConverter.class) Integer[] oldArray,
+                                                     @ConvertWith(IntegerArrayConverter.class) Integer[] newArray) {
+        Assertions.assertArrayEquals(newArray, Main.getNumbersFromArrayAfterLastFour(oldArray));
     }
 
     @CsvSource(value = {
@@ -26,9 +35,9 @@ public class MainTest {
             "1,4,4,1,1,4,3; false"
     }, delimiterString = ";")
     @ParameterizedTest
-    public void isArrayConsistsCertainNumbersTest(@ConvertWith(IntegerArrayConverter.class) Integer[] numbers,
-                                                  boolean isBoolean) {
-        Assertions.assertEquals(isBoolean, Main.isArrayConsistsCertainNumbers(numbers));
+    public void isThereOneAndFourInArrayTest(@ConvertWith(IntegerArrayConverter.class) Integer[] numbers,
+                                             boolean isBoolean) {
+        Assertions.assertEquals(isBoolean, Main.isThereOneAndFourInArray(numbers));
     }
 
 
